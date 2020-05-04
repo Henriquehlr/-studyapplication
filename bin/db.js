40f7035e7d8173914d0c4525b3770768c3698694
Your Name <you@example.com>
2020-05-03 21:07:51 -0300

    back

--- bin/db.js
+++ bin/db.js
@@ -1,4 +1,5 @@
 var conection = require("mongodb").MongoClient;
+var idEmpresa = require("mongodb").ObjectId;
 
 conection.connect("mongodb://localhost/empresa", { useUnifiedTopology: true })
     .then(conn => global.conn = conn.db("empresa"))
@@ -8,4 +9,22 @@ function findAll(callback){
     global.conn.collection("dadosempresa").find({}).toArray(callback);
 }
 
-module.exports = { findAll }
+function inserir(empresa, callback){
+    global.conn.collection("dadosempresa").insertOne(empresa, callback);
+}
+
+function buscarPorId(id, callback){
+    global.conn.collection("dadosempresa").find(new idEmpresa(id)).toArray(callback);
+}
+
+function atualizar(id, empresa, callback){
+        global.conn.collection("dadosempresa")
+            .updateOne({_id: new idEmpresa(id)},
+                       {$set: {
+                            nome: empresa.nome,
+                            numero: empresa.numero
+                       }}, callback);
+}
+
+
+module.exports = { findAll, inserir, buscarPorId, atualizar }
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,44 @@ router.get('/', function(req, res) {
   })
 });
 
-router.get('/new',function(req, res, next) {
-  res.render('new', { title: 'novo cadastro'});
-})
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Novo Cadastro',
+                    dados: {nome: "", numero: ""},
+                    action: 'new'
+  });
+});
+
+router.post('/new',function(req, res, next) {
+  var nome = req.body.nome;
+  var numero = parseInt(req.body.numero);
+  global.db.inserir({nome, numero}, (error) => {
+    if(error){
+      return console.log(error);
+    }res.redirect('/');
+  })
+
+router.get('/edit/:id', function(req, res){
+  var id = req.params.id;
+  global.db.buscarPorId(id, (error, docs) => {
+    if(error){
+      return console.log(error);
+    }
+    res.render('new', {title: 'Editar Empresa',
+                dados: docs[0],
+                action:  '/edit/' + docs[0]._id});
+    })
+  })
+
+  router.post('/edit/:id', function(req, res){
+    var id = req.params.id;
+    var nome = req.body.nome;
+    var numero = parseInt(req.body.numero);
+    global.db.atualizar(id, {nome, numero}, (error) => {
+      if(error){
+        return console.log(error);
+      }res.redirect('/');
+    })
+  })
+});
 
 module.exports = router;
